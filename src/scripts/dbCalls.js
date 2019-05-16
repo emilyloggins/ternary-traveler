db41d137ad8b14c11c5d01a8a3ee468f0cd76950
Emily Loggins <em.loggins7@gmail.com>
2019-05-16 10:18:06 -0500

    completed new interest form and appended all interests to the DOM

--- src/scripts/dbCalls.js
+++ src/scripts/dbCalls.js
@@ -3,7 +3,46 @@ const baseURL = "http://0.0.0.0:8088";
 const APIManager = {
     getAllPlaces: function() {
         return fetch(`${baseURL}/places`)
-                .then(response => response.json())
+            .then(response => response.json())
+    },
+    getOnePlace: function (id) {
+        return fetch(`${baseURL}/places?id=${id}`)
+            .then(response => response.json())
+    },
+    getAllInterests: function () {
+        return fetch(`${baseURL}/interests`)
+            .then(response => response.json())
+    },
+    getOneInterest: function (id) {
+        return fetch(`${baseURL}/interests?id=${id}`)
+            .then(response => response.json())
+    },
+    editInterest: function (id, obj) {
+        return fetch(`${baseURL}/interests?id=${id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(obj)
+        })
+    },
+    deleteInterest: function (id) {
+        return fetch(`${baseURL}/interests?id=${id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+    },
+    saveInterest: function (obj) {
+        return fetch(`${baseURL}/interests`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(obj)
+        })
+            .then(response => response.json())
     }
 }
 
